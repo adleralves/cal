a2766a0646a2e820697a8fd53a750deb6b33022e
adleralves <adler.alves@hotmail.com>
2022-09-28 10:49:09 -0300

    Criação do objeto Sala - v1.3

---
+++ src/main/java/com/adler/apical/domain/model/Sala.java
@@ -0,0 +1,12 @@
+package com.adler.apical.domain.model;
+
+/**
+ *
+ * @author adler
+ */
+public class Sala {
+    private Long id;
+    private String nome;
+    private boolean estado;
+    private String mac_address;
+}
