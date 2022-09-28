700a740e0d412650019864499d7f8420722f53af
adleralves <adler.alves@hotmail.com>
2022-09-28 10:47:20 -0300

    Criação do objeto Acessos - v1.2

---
+++ src/main/java/com/adler/apical/domain/model/Acessos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adler.apical.domain.model;
+
+/**
+ *
+ * @author adler
+ */
+public class Acessos {
+    private Long id;
+    private boolean verificacao;
+}
