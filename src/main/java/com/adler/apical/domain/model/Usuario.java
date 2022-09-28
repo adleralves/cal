85dc4640b3b47aa6389902a6c87d50e8caa856a9
adleralves <adler.alves@hotmail.com>
2022-09-28 11:24:54 -0300

    Instalação de dependencias e configuração do objeto Usuario - v1.4

--- src/main/java/com/adler/apical/domain/model/Usuario.java
+++ src/main/java/com/adler/apical/domain/model/Usuario.java
@@ -1,11 +1,93 @@
 package com.adler.apical.domain.model;
 
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
 /**
  *
  * @author adler
  */
+@Entity
 public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String login;
     private String senha;
+    private Long acessos_id;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String login, String senha, Long acessos_id) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.acessos_id = acessos_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Long getAcessos_id() {
+        return acessos_id;
+    }
+
+    public void setAcessos_id(Long acessos_id) {
+        this.acessos_id = acessos_id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
