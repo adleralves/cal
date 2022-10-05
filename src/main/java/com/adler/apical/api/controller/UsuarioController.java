093670a0db923b4d517693d2b13c72c848712dcf
adleralves <adler.alves@hotmail.com>
2022-10-05 09:39:32 -0300

    Métodos de buscar por id e deletar usuario funcionando - v1.13

--- src/main/java/com/adler/apical/api/controller/UsuarioController.java
+++ src/main/java/com/adler/apical/api/controller/UsuarioController.java
@@ -4,10 +4,14 @@ import com.adler.apical.domain.model.Usuario;
 import com.adler.apical.domain.repository.UsuarioRepository;
 import com.adler.apical.domain.service.UsuarioService;
 import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,17 +31,41 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     // Endpoint listando todos usuarios
-    
-    @GetMapping("/usuario")
+    @GetMapping("/usuarios")
     public List<Usuario> listar() {
         return usuarioRepository.findAll();
     }
 
+    // Endpoint que lista por id
+    @GetMapping("usuarios/{usuarioID}")
+    public ResponseEntity<Usuario> buscar(@PathVariable Long usuarioID) {
+        Optional<Usuario> usuario = usuarioRepository.findById(usuarioID);
+
+        //Verifica se usuario existe ou não
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     // Endpoint para adicionar novo usuario
-    
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario adicionar(@Valid @RequestBody Usuario usuario) {
         return usuarioService.salvar(usuario);
+    }
+
+    // Endpoint para excluir usuario
+    @DeleteMapping("/usuarios/{usuarioID}")
+    public ResponseEntity<Void> excluir(@PathVariable Long usuarioID) {
+        
+        //Verifica se usuario existe ou não
+        if (!usuarioRepository.existsById(usuarioID)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        usuarioService.excluir(usuarioID);
+        return ResponseEntity.noContent().build();
     }
 }
