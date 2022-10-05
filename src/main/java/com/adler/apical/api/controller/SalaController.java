141226c9164dc10aeff34dffe6226e3dad465b9a
adleralves <adler.alves@hotmail.com>
2022-10-05 10:38:13 -0300

    SalaController pronto - v1.15

--- src/main/java/com/adler/apical/api/controller/SalaController.java
+++ src/main/java/com/adler/apical/api/controller/SalaController.java
@@ -5,10 +5,34 @@
  */
 package com.adler.apical.api.controller;
 
+import com.adler.apical.domain.model.Sala;
+import com.adler.apical.domain.repository.SalaRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
- * @author devsys-a
+ * @author adler
  */
+@RestController
 public class SalaController {
     
+    @Autowired
+    private SalaRepository salaRepository;
+    
+    @GetMapping("/sala/{salaID}")
+    public ResponseEntity<Sala> listar(@PathVariable Long salaID) {
+        Optional<Sala> sala = salaRepository.findById(salaID);
+
+        //Verifica se usuario existe ou não
+        if (sala.isPresent()) {
+            return ResponseEntity.ok(sala.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
