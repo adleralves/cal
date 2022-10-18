fbe9d76e9938475c6f0cf658d476d61887f86fd4
adleralves <adler.alves@hotmail.com>
2022-10-18 13:44:58 -0300

    Puxando dados e colocando na tabela / teste - v1.20

--- src/main/java/com/adler/apical/api/controller/IndexController.java
+++ src/main/java/com/adler/apical/api/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.adler.apical.api.controller;
 
+import com.adler.apical.domain.repository.AcessosRepository;
+import com.adler.apical.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,13 +11,36 @@ import org.springframework.web.servlet.ModelAndView;
  *
  * @author adler
  */
-
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
     
+    @Autowired
+    private AcessosRepository acessoRepository;
+
     @GetMapping("/cadastro")
     public ModelAndView getTeste() {
         ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+    /*
+    @GetMapping("/usuarios/listar")
+    public ModelAndView listarUsuario() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        mv.addObject("usuarioList", usuarioRepository.findAll());
+        mv.addObject("acessoList", acessoRepository.findAll());
+        return mv;
+    }
+    */
+    @GetMapping("/usuarios/listar")
+    public ModelAndView listarUsuario() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        mv.addObject("acessoList", acessoRepository.findAll());
         return mv;
     }
 }
