160ed495e3edbbd18550c01c0a9ca9df304b910c
adleralves <adler.alves@hotmail.com>
2022-10-25 08:59:18 -0300

    Botão Novo usuário implementado e funcionando - v1.24

--- src/main/java/com/adler/apical/api/controller/IndexController.java
+++ src/main/java/com/adler/apical/api/controller/IndexController.java
@@ -1,10 +1,12 @@
 package com.adler.apical.api.controller;
 
+import com.adler.apical.domain.model.Usuario;
 import com.adler.apical.domain.repository.AcessosRepository;
 import com.adler.apical.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,17 +18,19 @@ public class IndexController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-    
+
     @Autowired
     private AcessosRepository acessoRepository;
 
+    /*
     @GetMapping("/cadastro")
     public ModelAndView getTeste() {
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
+     */
 
-    /*
+ /*
     @GetMapping("/usuarios/listar")
     public ModelAndView listarUsuario() {
         ModelAndView mv = new ModelAndView();
@@ -35,12 +39,19 @@ public class IndexController {
         mv.addObject("acessoList", acessoRepository.findAll());
         return mv;
     }
-    */
-    @GetMapping("/usuarios/listar")
+     */
+    @GetMapping("/admin")
     public ModelAndView listarUsuario() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
         mv.addObject("acessoList", acessoRepository.findAll());
+        return mv;
+    }
+
+    @GetMapping("/cadastro")
+    public ModelAndView cadastroAluno(@ModelAttribute Usuario usuario) {
+        ModelAndView mv = new ModelAndView("forms/usuarioForm");
+        mv.addObject("usuario", usuario);
         return mv;
     }
 }
