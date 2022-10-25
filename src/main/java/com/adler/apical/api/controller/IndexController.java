d71dd3a09eeb35f6d2be8116a902c205bcad0b42
adleralves <adler.alves@hotmail.com>
2022-10-25 15:30:41 -0300

    Form de usuario funcionando e mandando dados para o banco - v1.26

--- src/main/java/com/adler/apical/api/controller/IndexController.java
+++ src/main/java/com/adler/apical/api/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -22,24 +23,6 @@ public class IndexController {
     @Autowired
     private AcessosRepository acessoRepository;
 
-    /*
-    @GetMapping("/cadastro")
-    public ModelAndView getTeste() {
-        ModelAndView mv = new ModelAndView("index");
-        return mv;
-    }
-     */
-
- /*
-    @GetMapping("/usuarios/listar")
-    public ModelAndView listarUsuario() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("index");
-        mv.addObject("usuarioList", usuarioRepository.findAll());
-        mv.addObject("acessoList", acessoRepository.findAll());
-        return mv;
-    }
-     */
     @GetMapping("/admin")
     public ModelAndView listarUsuario() {
         ModelAndView mv = new ModelAndView();
@@ -52,6 +35,14 @@ public class IndexController {
     public ModelAndView cadastroAluno(@ModelAttribute Usuario usuario) {
         ModelAndView mv = new ModelAndView("forms/usuarioForm");
         mv.addObject("usuario", usuario);
+        return mv;
+    }
+
+    @PostMapping("criarUsuario")
+    public ModelAndView criarUsuario(Usuario usuario) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:index");
+        usuarioRepository.save(usuario);
         return mv;
     }
 }
